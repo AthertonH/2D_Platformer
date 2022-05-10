c8a48750422e3fabc2eb8e04413f6901de7fc476
Harrison <athertonharrison@gmail.com>
2022-05-09 23:29:50 -0700

    Initial commit

---
+++ settings.py
@@ -0,0 +1,16 @@
+level_map = [
+"                            ",
+"                            ",
+"                            ",
+" XX     XXX             XX  ",
+"XXXX            XXX      XX ",
+"XXX                         ",
+"XXX                         ",
+"XX     X     XX    XX   XX  ",
+"       X  XXXXX    XX   XXX ",
+"    XXXX  XXXXX    XX   XXXX",
+"XXXXXXXX  XXXXX    XX   XXXX",]
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
