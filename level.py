bfbfebbad0e27dea266b2d2ec70e6042da9bac3e
Harrison <athertonharrison@gmail.com>
2022-05-12 20:31:02 -0700

    40:12

--- level.py
+++ level.py
@@ -1,6 +1,6 @@
 import pygame
 from tile import Tile
-from settings import tile_size
+from settings import tile_size, screen_width
 from player import Player
 
 
@@ -30,21 +30,46 @@ class Level:
         player_x = player.rect.centerx
         direction_x = player.direction.x
 
-        if player_x < 200 and direction_x < 0:
+        if player_x < (screen_width / 4) and direction_x < 0:
             self.world_shift = 8
             player.speed = 0
-        elif player_x > 1000 and direction_x > 0:
+        elif player_x > screen_width - (screen_width / 4) and direction_x > 0:
             self.world_shift = -8
             player.speed = 0
         else:
             self.world_shift = 0
             player.speed = 8
 
+    def horizontal_movement_collision(self):
+        player = self.player.sprite
+        player.rect.x += player.direction.x * player.speed
+
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.direction.x < 0:
+                    player.rect.left = sprite.rect.right
+                elif player.direction.x > 0:
+                    player.rect.right = sprite.rect.left
+
+########## LEFT OFF HERE
+    def vertical_movement_collision(self):
+        player = self.player.sprite
+        player.rect.y += player.direction.y
+
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.direction.y < 0:
+                    player.rect.bottom = sprite.rect.top
+                elif player.direction.y > 0:
+                    player.rect.top = sprite.rect.bottom
+
     def run(self):
         # Level tiles
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
+        self.scroll_x()
         # Player
         self.player.update()
+        self.horizontal_movement_collision()
+        self.vertical_movement_collision()
         self.player.draw(self.display_surface)
-        self.scroll_x()
